226118545ea549cce5c4a5060526ddc5378bc89c
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-04-08 16:22:27 -0600

    UserClassroom entity

---
+++ Models/UserClassroom.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Waggle.Models
+{
+    public class UserClassroom
+    {
+        // Key Properties
+        public int UserClassroomID { get; set; }
+        public int UserId { get; set; }
+        public int ClassroomId { get; set; }
+        
+        // Standard Properties
+        public string Role { get; set; } // consider enum?
+        public string DisplayName { get; set; }
+        // public x ProfilePicture { get; set; }
+        
+        // Navigation Properties
+        public User User { get; set; }
+        public Classroom Classroom { get; set; }
+        // public ICollection<Achievement> Achievements { get; set; }
+        // public ICollection<Badge> Badges { get; set; }
+    }
+}
