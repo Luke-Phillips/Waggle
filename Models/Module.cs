8a4b8609523a474d8e329bdfbd7ce0a88890ff6d
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-04-08 16:54:27 -0600

    Module entity

---
+++ Models/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Waggle.Models
+{
+    public class Module
+    {
+        // Key Properties
+        public int ModuleID { get; set; }
+
+        // Regular Properties
+        public string Name { get; set; }
+
+        // Navigation Properties
+        public ICollection<Post> Posts { get; set; }
+    }
+}
