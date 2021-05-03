49e93f4a0e90cf5179a8a356c51308ecd23a7fa0
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-05-03 13:28:03 -0600

    Class-nav fetch

--- ClientApp/src/components/class-nav/class-nav.component.jsx
+++ ClientApp/src/components/class-nav/class-nav.component.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ClassNavIcon from '../class-nav-icon/class-nav-icon.component'
-import './class-nav.styles.scss'
+import React from 'react';
+import ClassNavIcon from '../class-nav-icon/class-nav-icon.component';
+import './class-nav.styles.scss';
 
-const ClassNav = (props) => (
-    <div className='class-nav'>
-        <ClassNavIcon />
+const ClassNav = props => {
+  const classrooms = fetch(`/api/userclassrooms/${props.userId}`)
+    .then(res => res.json());
+
+  return (
+    <div className="class-nav">
+      <ClassNavIcon />
     </div>
-);
+  );
+};
 
 export default ClassNav;
