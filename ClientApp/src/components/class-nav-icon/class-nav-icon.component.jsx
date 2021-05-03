e37e015c1a5e3beb2c9935ffcdb5769509f94bf1
Cade Gardner <gar17040@byui.edu>
2021-05-03 13:25:35 -0600

    edited class-nav to display icons based on number of classes the user has

--- ClientApp/src/components/class-nav-icon/class-nav-icon.component.jsx
+++ ClientApp/src/components/class-nav-icon/class-nav-icon.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { ReactComponent as Icon } from '../../assets/honey-jar.svg'
-import './class-nav-icon.styles.scss'
+import React from "react";
+import { ReactComponent as Icon } from "../../assets/honey-jar.svg";
+import "./class-nav-icon.styles.scss";
 
-const ClassNavIcon = () => (
-    <div className='class-nav-icon'>
-        <Icon className='icon'/>
-        <img src='../../assets/honey-jar.svg' alt=''></img>
-        <p>Science</p>
-    </div>
+const ClassNavIcon = (props) => (
+  <div className="class-nav-icon">
+    <Icon className="icon" />
+    <img src="../../assets/honey-jar.svg" alt=""></img>
+    <p>{props.classroom.name}</p>
+  </div>
 );
 
 export default ClassNavIcon;
--- ClientApp/src/components/class-nav/class-nav.component.jsx
+++ ClientApp/src/components/class-nav/class-nav.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ClassNavIcon from '../class-nav-icon/class-nav-icon.component'
-import './class-nav.styles.scss'
+import React from "react";
+import ClassNavIcon from "../class-nav-icon/class-nav-icon.component";
+import "./class-nav.styles.scss";
 
 const ClassNav = (props) => (
-    <div className='class-nav'>
-        <ClassNavIcon />
-    </div>
+  <div className="class-nav">
+    {props.classrooms.map((classroom) => (
+      <ClassNavIcon key={classroom.id} classroom={classroom} />
+    ))}
+  </div>
 );
 
 export default ClassNav;
