369c7278399c0da9fed96b9cdb60eb5e31a03d2a
Cade Gardner <gar17040@byui.edu>
2021-05-03 14:23:27 -0600

    edited class-nav to use hooks to make the api call, api call not working for me currently

--- ClientApp/src/components/class-nav/class-nav.component.jsx
+++ ClientApp/src/components/class-nav/class-nav.component.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ClassNavIcon from '../class-nav-icon/class-nav-icon.component';
-import './class-nav.styles.scss';
+import React, {useState, useEffect } from "react";
+import ClassNavIcon from "../class-nav-icon/class-nav-icon.component";
+import "./class-nav.styles.scss";
 
-const ClassNav = props => {
-  const classrooms = fetch(`/api/userclassrooms/${props.userId}`)
-    .then(res => res.json());
+const ClassNav = (props) => {
+  const[classrooms, setClassrooms] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/userclassrooms/${props.userId}`).then((res) =>
+    res.json()).then(response => { setClassrooms(response.items) })
+  });
+//   const classrooms = fetch(`/api/userclassrooms/${props.userId}`).then((res) =>
+//     res.json());
+
+  console.log(classrooms)
   return (
     <div className="class-nav">
-      {props.classrooms.map(classroom => (
-        <ClassNavIcon key={classroom.id} classroom={classroom} />
+      {classrooms.map((classroom) => (
+        <ClassNavIcon key={classroom.classroomID} classroom={classroom} />
       ))}
     </div>
   );
--- ClientApp/src/pages/discussion-page/discussion-page.component.jsx
+++ ClientApp/src/pages/discussion-page/discussion-page.component.jsx
@@ -3,10 +3,14 @@ import Question from '../../components/question/question.component'
 import ClassNav from '../../components/class-nav/class-nav.component'
 import './discussion-page.styles.scss'
 
+
+
 const DiscussionPage = () => (
+
+
     <div className='discussion-page'>
         <div>
-            <ClassNav />
+            <ClassNav userId='6'/>
         </div>
         <h1> Get ready to share </h1>
         <Question />
