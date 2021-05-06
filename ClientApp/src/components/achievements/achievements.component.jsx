405f05504af9e4aaa94011829b7b5a7b2662e3f3
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-05-06 15:12:20 -0600

    improved ach/badge stubs

--- ClientApp/src/components/achievements/achievements.component.jsx
+++ ClientApp/src/components/achievements/achievements.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './achievements.styles.scss';
 
 const Achievements = props => {
-  // achievements.map(achievement => (
+  const achievements = props.achievements.map(achievement => (
+    <li>achievement</li>
+  ));
 
-  // ))
-}
+  return achievements;
+};
 
 export default Achievements;
--- ClientApp/src/components/badges/badges.component.jsx
+++ ClientApp/src/components/badges/badges.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './badges.styles.scss';
 
 const Badges = props => {
+  const badges = props.badges.map(badge => <li>badge</li>);
 
-}
+  return badges;
+};
 
 export default Badges;
