b2a45315ec228176dab8088d2af8195e9e0a680e
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-05-11 11:57:55 -0600

    added to hive manager card stub

--- ClientApp/src/components/hive-manager-card/hive-manager-card.component.jsx
+++ ClientApp/src/components/hive-manager-card/hive-manager-card.component.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import './hive-manager-card.styles.scss';
 
 const HiveManagerCard = () => {
-
+  return <p>this will be the hive manager card</p>
 };
 
 export default HiveManagerCard;
