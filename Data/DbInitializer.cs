ac7106a6d56e2fa321d7f0463d6b39091857d0ea
Luke Phillips <luke.thomas.phillips@gmail.com>
2021-05-14 10:16:35 -0600

    connected to RDS from local machine

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -11,6 +11,7 @@ namespace Waggle.Data
         public static void Initialize(WaggleContext context)
         {
             //context.Database.EnsureCreated();
+            Console.WriteLine("About to attempt access to db.Users--------------------------------------");
 
             // Look for any existing users in the database
             if (context.Users.Any())
--- Startup.cs
+++ Startup.cs
@@ -17,7 +17,7 @@ namespace Waggle
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration;        
         }
 
         public IConfiguration Configuration { get; }
