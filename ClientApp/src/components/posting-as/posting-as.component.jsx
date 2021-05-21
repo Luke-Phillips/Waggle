adcf8c2c4b595095a8dfd8d2a339abe9de363779
Cade Gardner <gar17040@byui.edu>
2021-05-20 20:01:54 -0600

    modified styles to fix a few small issues on the discussion page.

--- ClientApp/src/components/posting-as/posting-as.component.jsx
+++ ClientApp/src/components/posting-as/posting-as.component.jsx
@@ -4,7 +4,7 @@ import './posting-as.styles.scss'
 
 const PostingAs = props => (
   <div className='posting-as'>
-    <p >{`Posting as ${props.type} as `}</p> 
+    <p>{`Posting ${props.type} as`}</p> 
     <CustomButton className='posting-as-button' value={props.user}>{props.user}</CustomButton>
     <CustomButton className='posting-as-button' value='anonymous'>Anonymous</CustomButton>
   </div>
