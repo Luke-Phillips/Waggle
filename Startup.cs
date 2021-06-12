7345cfabb49eb0cd00cfa35ab768396417863d5f
Cade Gardner <gar17040@byui.edu>
2021-06-12 17:53:28 -0600

    Changed RDSConnection to LocalConnection

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace Waggle
                 configuration.RootPath = "ClientApp/build";
             });
 
-            string mySqlConnectionStr = Configuration.GetConnectionString("RdsConnection"); // alternatively, LocalConnection
+            string mySqlConnectionStr = Configuration.GetConnectionString("LocalConnection"); // alternatively, LocalConnection
             services.AddDbContext<WaggleContext>(options =>
                     options.UseMySql(mySqlConnectionStr, ServerVersion.AutoDetect(mySqlConnectionStr)));
 
