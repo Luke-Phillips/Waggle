3c4f5c0c5cefa0e39880e2253a71d219dad6042b
Cade Gardner <gar17040@byui.edu>
2021-06-21 20:50:06 -0600

    installed material-ui/core in ClientApp folder for slider, created ratings component, modified clickable area to show reeplies column

--- ClientApp/src/components/discussion-feed-item/discussion-feed-item.component.jsx
+++ ClientApp/src/components/discussion-feed-item/discussion-feed-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import PostHeader from '../post-header/post-header.component';
 import CustomButton from '../custom-button/custom-button.component';
+import Ratings from '../ratings/ratings.component';
 
 import './discussion-feed-item.style.scss';
-
 
 const postTime = new Date('2021/5/17 16:24:00');
 // Maybe should use children for content
 const DiscussionFeedItem = ({ children, ...otherProps }) => (
-  <div id={otherProps.postType} className={`discussion-feed-item ${otherProps.postWidth}`} onClick={otherProps.onClick}>
+  <div
+    id={otherProps.postType}
+    className={`discussion-feed-item ${otherProps.postWidth}`}
+    
+  >
+    <div onClick={otherProps.onClick}>
     <PostHeader user={otherProps.user} date={postTime} />
     <p className='content'>{children}</p>
+    </div>
+
     <div className='footer'>
-      <input className='upload' type='file' />
-      <CustomButton className='feed-item-btn'>
-        {otherProps.btnName}
-      </CustomButton>
+      <Ratings />
+      <div className='btnContainer'>
+        <input className='upload' type='file' />
+        <CustomButton className='feed-item-btn'>
+          {otherProps.btnName}
+        </CustomButton>
+      </div>
     </div>
   </div>
 );
--- ClientApp/src/pages/discussion-page/discussion-page.component.jsx
+++ ClientApp/src/pages/discussion-page/discussion-page.component.jsx
@@ -124,6 +124,7 @@ const DiscussionPage = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </DiscussionFeedItem>
+          
         </div>
         
           <RepliesColumn className='visible'
