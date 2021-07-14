5e61a1458dafc5619dac0e81b148670d72053451
Cade Gardner <gar17040@byui.edu>
2021-07-13 20:23:39 -0600

    resolved conflict for real this time

--- ClientApp/src/components/class-nav-icon/class-nav-icon.component.jsx
+++ ClientApp/src/components/class-nav-icon/class-nav-icon.component.jsx
@@ -10,10 +10,6 @@ const ClassNavIcon = ({ classroom, handleClassSelect }) => {
   return (
     <div className='class-nav-icon' onClick={onClickHandler}>
       <Icon className='icon' />
-<<<<<<< HEAD
-      {/* <img src='../../assets/honey-jar.svg' alt=''></img> */}
-=======
->>>>>>> a5ae6cefdb979c9985426fff7e36b6f0bc70ded1
       <p>{classroom.name}</p>
     </div>
   );
